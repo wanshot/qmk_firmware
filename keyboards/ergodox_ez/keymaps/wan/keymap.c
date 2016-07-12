6c45e211fc545e4ce803d2523cb8a798eec6fcd7
wan <nishikawa0228@sj9.so-net.ne.jp>
2016-07-13 00:22:39 +0900

    update

--- keyboards/ergodox_ez/keymaps/wan/keymap.c
+++ keyboards/ergodox_ez/keymaps/wan/keymap.c
@@ -10,15 +10,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   '"   |   1  |   2  |   3  |   4  |   5  |  6   |           |  7   |   8  |   9  |   0  |   (  |   )  |   =    |
+ * |   !    |   1  |   2  |   3  |   4  |   5  |  6   |           |  7   |   8  |   9  |   0  |   (  |   )  |   =    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  TAB   |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   -    |
+ * |  TAB   |   Q  |   W  |   E  |   R  |   T  |  '"  |           |  ~   |   Y  |   U  |   I  |   O  |   P  |   -    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |  CTRL  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   [{   |
  * |--------+------+------+------+------+------| EISU |           | KANA |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |   ]}   |
+ * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |]}/Shift|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  @   |Grv/L1|AltShf|   \  |   !  |                                       | Left | Down |  Up  |Right | ~L1  |
+ *   |  @   |Grv/L1|AltShf|   \  |   `  |                                       | Left | Down |  Up  |Right | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | LGUI | END  |       | Alt  | Ctrl  |
@@ -32,19 +32,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_QUOT,        KC_1,              KC_2,           KC_3,   KC_4,   KC_5,   KC_6,
-        KC_TAB,         KC_Q,              KC_W,           KC_E,   KC_R,   KC_T,   TG(SYMB),
+        KC_EXLM,        KC_1,              KC_2,           KC_3,   KC_4,   KC_5,   KC_6,
+        KC_TAB,         KC_Q,              KC_W,           KC_E,   KC_R,   KC_T,   KC_QUOT,
         CTL_T(KC_ESC),  KC_A,              KC_S,           KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),       KC_X,           KC_C,   KC_V,   KC_B,   ALL_T(KC_LANG2),
-        KC_AT,          LT(SYMB,KC_GRV),   LALT(KC_LSFT),  KC_BSLS,KC_EXLM,
+        KC_AT,          LT(SYMB,KC_GRV),   LALT(KC_LSFT),  KC_BSLS,KC_GRV,
                                            KC_LGUI,        KC_END,
                                                                    KC_HOME,
                                            KC_SPC,         KC_BSPC,KC_LGUI,
         // right hand
              KC_7,           KC_8,   KC_9,    KC_0,    KC_LPRN, KC_RPRN,          KC_EQL,
-             TG(SYMB),       KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,             KC_MINS,
+             KC_TILD,        KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,             KC_MINS,
                              KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN,          KC_LBRC,
-             MEH_T(KC_LANG1),KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RBRC,
+             MEH_T(KC_LANG1),KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          SFT_T(KC_RBRC),
                                   	 KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,         KC_FN1,
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
